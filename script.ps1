69684403d8fec8895697985dd93e4d984b5d7e2f
sgarad0123 <162799641+sgarad0123@users.noreply.github.com>
2024-09-29 22:40:01 +0530

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,7 @@
+﻿$password = ConvertTo-SecureString "9403" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ("shubham", $password)
+New-SSHSession -ComputerName "DESKTOP-8R1FAH3" -Credential $credential
+
+
+# Optionally, run a command on the remote system
+Invoke-SSHCommand -Index 0 -Command "echo 'mc' "
